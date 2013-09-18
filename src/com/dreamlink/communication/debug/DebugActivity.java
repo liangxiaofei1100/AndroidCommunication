2d1ddf1e3c6afd995ce7d516fe2dd18999693e93
alex_liang <liangxiaofei1100@163.com>
2013-09-18 15:51:10 +0800

    20130918 add about activity.

---
+++ src/com/dreamlink/communication/debug/DebugActivity.java
@@ -0,0 +1,13 @@
+package com.dreamlink.communication.debug;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DebugActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setTitle("Debug");
+	}
+}
